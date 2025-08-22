195d5d5f19c0a887e52b87819e4ff4f2720012c4
David Herman <bitspittle@gmail.com>
2025-08-22 10:41:04 -0700

    Remove JDK24 warnings

--- examples/pong/build.gradle.kts
+++ examples/pong/build.gradle.kts
@@ -9,7 +9,23 @@ version = "0.0.1"
 
 kotlin {
     @OptIn(ExperimentalKotlinGradlePluginApi::class)
-    jvm { binaries { executable { mainClass.set("MainKt") } } }
+    jvm {
+        binaries {
+            executable {
+                mainClass.set("MainKt")
+                applicationDefaultJvmArgs.addAll(
+                    // JDK24 started reporting warnings for libraries that use restricted native methods, which Kross2d
+                    // definitely does with its sound library:
+                    //
+                    // The best solution we have for now is to disable the warning by explicitly enabling access.
+                    // We also suggest the IgnoreUnrecognizedVMOptions flag here to allow applications to be able to
+                    // compile with JDKs older than JDK24. You can remove it if you are intentionally using JDK24+.
+                    "-XX:+IgnoreUnrecognizedVMOptions",
+                    "--enable-native-access=ALL-UNNAMED",
+                )
+            }
+        }
+    }
     js { browser { binaries.executable() } }
     sourceSets {
         commonMain.dependencies {
