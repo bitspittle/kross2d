30982d3420aa31dd1a7d34ffed6d008f42536896
David Herman <bitspittle@gmail.com>
2025-08-23 12:42:35 -0700

    Fix ArenaSize

--- examples/pong/src/jvmMain/kotlin/main.kt
+++ examples/pong/src/jvmMain/kotlin/main.kt
@@ -2,7 +2,7 @@ import dev.bitspittle.kross2d.engine.app.AppParams
 import dev.bitspittle.kross2d.engine.app.launch
 
 fun main() {
-    val screenSize = PongState.ARENA_SIZE * 5f
+    val screenSize = PongState.ArenaSize * 5f
     launch(
         AppParams("Pong", screenSize),
         initialState = PongState())
