0aab484126e914c49b088f3b75f8f69e2149c001
David Herman <bitspittle@gmail.com>
2025-08-25 10:55:24 -0700

    Nit: Make `synchronized` internal consistently across all targets
    (Otherwise it shows up in docs)

--- kross2d/core/src/jvmMain/kotlin/dev/bitspittle/kross2d/core/concurrency/Synchronized.jvm.kt
+++ kross2d/core/src/jvmMain/kotlin/dev/bitspittle/kross2d/core/concurrency/Synchronized.jvm.kt
@@ -1,3 +1,3 @@
 package dev.bitspittle.kross2d.core.concurrency
 
-actual inline fun <T> synchronized(lock: Any, block: () -> T): T = kotlin.synchronized(lock, block)
+internal actual inline fun <T> synchronized(lock: Any, block: () -> T): T = kotlin.synchronized(lock, block)
