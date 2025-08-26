7ab8382ad93b2ed0fbca9028369dcf2cfdc02925
David Herman <bitspittle@gmail.com>
2025-08-25 21:02:45 -0700

    Split Asset / AssetLoader into two files

---
+++ kross2d/core/src/commonMain/kotlin/dev/bitspittle/kross2d/engine/assets/Asset.kt
@@ -0,0 +1,82 @@
+package dev.bitspittle.kross2d.engine.assets
+
+import dev.bitspittle.kross2d.core.event.Event
+import dev.bitspittle.kross2d.core.event.ObservableEvent
+import dev.bitspittle.kross2d.core.memory.Disposable
+import dev.bitspittle.kross2d.core.memory.Disposer
+import dev.bitspittle.kross2d.core.memory.ImmutableDisposable
+import dev.bitspittle.kross2d.core.memory.use
+
+/**
+ * A handle to an asset that will get loaded asynchronously.
+ *
+ * If [data] is non-null, this particular asset loaded successfully. Otherwise, the caller may
+ * want to check [state] to see if any asset failed to load, which could be particularly useful for
+ * logging errors / aborting the game.
+ */
+class Asset<D: Disposable>(parent: ImmutableDisposable, val path: String) : Disposable(parent) {
+    enum class State {
+        LOADING,
+        LOADED,
+        FAILED,
+    }
+
+    var state = State.LOADING
+        private set(value) {
+            field = value
+            if (field == State.LOADED) {
+                fireOnLoaded(this.data!!)
+            }
+            _onLoaded.clear()
+        }
+
+    private val _onLoaded = Event<D> { data?.let { fireOnLoaded(it) } }
+    val onLoaded: ObservableEvent<D> = _onLoaded
+
+    /**
+     * The value of this handle. Will only be non-null if the current [state] is [State.LOADED]
+     */
+    var data: D? = null
+        private set
+
+    internal fun setData(data: D?) {
+        if (this.disposed) {
+            // It's possible that this asset shell already got disposed by the time we finished
+            // loading the underlying data. This probably won't happen in practice, but just in
+            // case, let's handle it by consuming the data.
+            data?.use {}
+            return
+        }
+
+        assertLoading()
+        if (data != null) {
+            Disposer.reparent(this, data)
+            this.data = data
+            state = State.LOADED
+        }
+        else {
+            state = State.FAILED
+        }
+    }
+
+    private fun fireOnLoaded(data: D) {
+        _onLoaded(data)
+        _onLoaded.clear()
+    }
+
+    internal fun notifyFailure() {
+        if (this.disposed) {
+            // We got disposed before the target data had a chance to fail loading. This probably
+            // won't happen in practice, but if it does, who cares! Let's just ignore it.
+            return
+        }
+        assertLoading()
+        state = State.FAILED
+    }
+
+    private fun assertLoading() {
+        if (state != State.LOADING) {
+            throw IllegalStateException("Attempting to modify frozen AssetHandle")
+        }
+    }
+}
